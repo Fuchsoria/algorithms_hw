686afef86fa4d23a2e5d3a8fa96eb313595c5424
German <fuchsoria@protonmail.ch>
2022-02-21 22:34:05 +0100

    Init hw06

---
+++ 06_sort/main.go
@@ -0,0 +1,19 @@
+package main
+
+type Sort struct {}
+
+func (s *Sort) Bubble(arr int[]) int[] {
+
+}
+
+func (s *Sort) Insertion(arr int[]) int[] {
+
+}
+
+func (s *Sort) Shell(arr int[]) int[] {
+
+}
+
+func main() {
+	
+}
